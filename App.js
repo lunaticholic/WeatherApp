c98aa51785a08b894c63296ee27f507545003369
lunaticholic <lunaticholic@gmail.com>
2021-04-24 21:30:44 +0400

    1.3 Loading Screen - 생애 첫 React Native 첫화면 w/ Expo

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView} />
-      <View style={styles.blueView} />
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  // Container에서 flex 1을 사용해야만 비로소 전체공간을 사용할 수 있다.
-  container: {
-    flex: 1
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow"
-  },
-  blueView: {
-    flex: 1,
-    backgroundColor: "blue"
-  }
-});
